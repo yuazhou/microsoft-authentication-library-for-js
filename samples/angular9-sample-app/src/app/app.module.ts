8b8f6b01339f4e49cfd58006fd4243985f904ac2
Jason Nutter <janutter@microsoft.com>
2020-03-02 16:06:10 -0800

    Example of dynamic configuration for Angular

--- samples/angular9-sample-app/src/app/app.module.ts
+++ samples/angular9-sample-app/src/app/app.module.ts
@@ -13,52 +13,10 @@ import { ProfileComponent } from './profile/profile.component';
 import {
   MsalModule,
   MsalInterceptor,
-  MSAL_CONFIG,
-  MSAL_CONFIG_ANGULAR,
   MsalService,
-  MsalAngularConfiguration
 } from '@azure/msal-angular';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
-import { Configuration } from 'msal';
-
-export const protectedResourceMap: [string, string[]][] = [
-  ['https://graph.microsoft.com/v1.0/me', ['user.read']]
-];
-
-const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
-
-function MSALConfigFactory(): Configuration {
-  return {
-    auth: {
-      clientId: '6226576d-37e9-49eb-b201-ec1eeb0029b6',
-      authority: "https://login.microsoftonline.com/common/",
-      validateAuthority: true,
-      redirectUri: "http://localhost:4200/",
-      postLogoutRedirectUri: "http://localhost:4200/",
-      navigateToLoginRequestUrl: true,
-    },
-    cache: {
-      cacheLocation: "localStorage",
-      storeAuthStateInCookie: isIE, // set to true for IE 11
-    },
-  };
-}
-
-function MSALAngularConfigFactory(): MsalAngularConfiguration {
-  return {
-    popUp: !isIE,
-    consentScopes: [
-      "user.read",
-      "openid",
-      "profile",
-      "api://a88bb933-319c-41b5-9f04-eff36d985612/access_as_user"
-    ],
-    unprotectedResources: ["https://www.microsoft.com/en-us/"],
-    protectedResourceMap,
-    extraQueryParameters: {}
-  };
-}
 
 @NgModule({
   declarations: [
@@ -82,14 +40,6 @@ function MSALAngularConfigFactory(): MsalAngularConfiguration {
       provide: HTTP_INTERCEPTORS,
       useClass: MsalInterceptor,
       multi: true
-    },
-    {
-      provide: MSAL_CONFIG,
-      useFactory: MSALConfigFactory
-    },
-    {
-      provide: MSAL_CONFIG_ANGULAR,
-      useFactory: MSALAngularConfigFactory
     },
     MsalService
   ],
--- samples/angular9-sample-app/src/main.ts
+++ samples/angular9-sample-app/src/main.ts
@@ -3,10 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic/platfo
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { MSAL_CONFIG, MSAL_CONFIG_ANGULAR } from '@azure/msal-angular';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+fetch('/assets/configuration.json')
+  .then(response => response.json())
+  .then(json => {
+    platformBrowserDynamic([
+      { provide: MSAL_CONFIG, useValue: json.msal },
+      { provide: MSAL_CONFIG_ANGULAR, useValue: json.angular }
+    ])
+      .bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  });
